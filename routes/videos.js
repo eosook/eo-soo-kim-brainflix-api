4efedc5ab9bdc766cf24b7a8f120eccf13e36c33
Eo Soo Kim <eosookim@gmail.com>
2024-06-10 17:18:56 -0400

    create route for videoId

--- routes/videos.js
+++ routes/videos.js
@@ -15,4 +15,12 @@ videoRouter
         res.json(videosData);
     })
 
+videoRouter
+    .route('./:videoId')
+    .get((req, res) =>{
+        const videoIdParam = req.params.videoId;
+        const videosData = readVideos();
+        const foundVideo = videosData.find((video) => video.id === videoIdParam);
+    })
+
 export default videoRouter;
